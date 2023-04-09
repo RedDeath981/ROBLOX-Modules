b1a5ab8d5c92ae7bf5b8281ad1bcef283997e066
RedDeath981 <130311879+RedDeath981@users.noreply.github.com>
2023-04-09 16:29:01 -0400

    Create Example.lua

---
+++ Easy Tweening/Example.lua
@@ -0,0 +1,41 @@
+--// Variables \\--
+local EasyTweening = require(game.ReplicatedStorage.TweenModule)
+local TweenInformation = TweenInfo.new(
+		5, -- Time
+		Enum.EasingStyle.Linear, -- Easing Style
+		Enum.EasingDirection.InOut, -- Easing Direction
+		0, -- Repeat Amount
+		false, -- Reverse
+		7 -- Delay
+)
+local Goals = {
+    CFrame = workspace.Baseplate.CFrame * CFrame.new(0, 2, 0)
+}
+local Object = workspace.Baseplate
+
+--// Methods \\--
+local EasyTweeningNew = EasyTweening:new('Baseplate Tween') -- Thread Name
+local Tween = EasyTweeningNew:CreateTween(Object, TweenInformation, Goals):play(4):started(function(Data) 
+		print('Tween Started [Invoked :play()]', Data) 
+	end, "!"):completed(function(Playback, Number)
+		print('Tween Completed [Tween Ended]', Number)
+	end, 1):playing(function(Playback, InstanceName)
+		print("Tween Playing [While it's running]", InstanceName)
+	end, Object.Name):delayed(function(Playback, String)
+		print("Tween Delayed [Hasn't ran yet due to \"Delay\" variable in TweenInfo", String)
+	end, "Hello"):begin(function(Playback, Func)
+		print("Tween Began [Keeps running before :Play() invoked")
+		Func()
+	end, function()
+		print('This is the "Func" variable in :begin()') 
+	end):getTween()
+
+print(Tween.Instance) -- Prints "Baseplate"
+
+for Index, ThreadData in pairs(Mod.GetThreads()) do -- Gets all threads
+	print(Index, ThreadData)
+end
+
+for Index, ThreadData in pairs(Mod.GetThreadData("Baseplate Tween")) do -- Gets Tween off Thread Name
+  print(Index, ThreadData)
+end
